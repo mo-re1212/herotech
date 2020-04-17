6d7079cfc30ea98953c92f82c1b59421a7bd0c7d
mo-re1212 <mohammadrezaashtari1@gmail.com>
2020-04-17 19:40:02 +0430

    Initial Commit

---
+++ newproject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  newproject
+//
+//  Created by Apple on 4/17/20.
+//  Copyright © 2020 mreza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
